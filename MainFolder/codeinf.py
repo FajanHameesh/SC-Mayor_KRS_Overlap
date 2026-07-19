541074c0ad95700862498a0b755e24b85c13edea
FajanHameesh <fajanhameesh@gmail.com>
2026-07-19 07:03:28 +0700

    Cache
    Added Streamlit caching to codeinf function.

--- MainFolder/codeinf.py
+++ MainFolder/codeinf.py
@@ -1,6 +1,8 @@
 import requests
+import streamlit as st
 base_url = 'https://krs.ipb.ac.id/api/DetailMataKuliah?kurikulumId='
 
+@st.cache_data(max_entries=2)
 def codeinf(id):
     use_url = base_url+str(id)
     response = requests.get(use_url)
