16b114eeb5081f59f7d04d9aa44acaa4f11112ba
FajanHameesh <fajanhameesh@gmail.com>
2026-07-19 07:01:39 +0700

    Enhance GUI with semester selection and banner
    Refactor GUI to include semester selection and display banner image.

--- MainFolder/gui.py
+++ MainFolder/gui.py
@@ -3,46 +3,70 @@ from .maincode1 import scrapper
 from .maincode2 import runn
 from .codeinf import codeinf
 from .listMK import listMK
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent
+
 def gui():
+    st.image(base_dir/"banner.png", use_container_width=False)
 
     st.title("SC-Mayor Overlap Tracker awkwkw")
 
-    # Input user
-    bound1 = st.number_input(
-        "ID Ganjil Lower",
-        min_value=0,
-        value=192730
+    semester = st.selectbox(
+        "Semester: ",
+        ['Ganjil','Genap']
     )
-    st.info(f"{codeinf(bound1)}")
 
-    bound2 = st.number_input(
-        "ID Ganjil Upper",
-        min_value=0,
-        value=192738
-    )
-    st.info(f"{codeinf(bound2)}")
+    col1, col2 = st.columns(2)
 
-    bound3 = st.number_input(
-        "ID Genap Lower",
-        min_value=0,
-        value=196041
-    )
-    st.info(f"{codeinf(bound3)}")
+    if semester == 'Ganjil':
+        bound3 = 196041
+        bound4 = bound3
 
-    bound4 = st.number_input(
-        "ID Genap Upper",
-        min_value=0,
-        value=196041
-    )
-    st.info(f"{codeinf(bound4)}")
+        with col1:
+            bound1 = st.number_input(
+            "ID paling bawah matkul semester terkait",
+            min_value=0,
+            value=192730
+        )
+            st.info(f"{codeinf(bound1)}")
+
+        with col2:
+            bound2 = st.number_input(
+            "ID paling atas matkul semester terkait",
+            min_value=0,
+            value=192738
+        )
+            st.info(f"{codeinf(bound2)}")  
+
+    elif semester == 'Genap':      
+        bound1 = 196041
+        bound2 = bound1
+        with col1:
+            bound3 = st.number_input(
+            "ID paling bawah matkul semester terkait",
+            min_value=0,
+            value=196041
+        )
+            st.info(f"{codeinf(bound3)}")
+
+        with col2:
+            bound4 = st.number_input(
+            "ID paling atas matkul semester terkait",
+            min_value=0,
+            value=196041
+        )
+            st.info(f"{codeinf(bound4)}")
 
     listmk = listMK()
     bound5_raw = st.selectbox(
         "Matkul SC",
-        listmk
+        listmk.keys()
     )
-    bound5,_ = str(bound5_raw).split('-',maxsplit=1)
-    bound5 = int(bound5)
+    bound5 = listmk[bound5_raw]
+    st.info(
+        f'[Info SC](https://krs.ipb.ac.id/MK/{str(bound5)})'
+        )
 
     paralell = st.number_input(
         "Paralel",
@@ -56,7 +80,7 @@ def gui():
         with st.spinner("Mengambil data..."):
             df1 = scrapper(bound1,bound2+1,bound3,bound4)
             df2 = scrapper(bound5,bound5+1,bound5,bound5)
-            df3,df3_2 = runn(df1,df2,str(paralell))
+            df3,df3_2,df3_3 = runn(df1,df2,str(paralell))
         st.success("Selesai!")
 
         # Menampilkan dataframe
@@ -70,4 +94,13 @@ def gui():
         st.dataframe(df3)
 
         st.subheader("Non-Overlap Paralel/s")
-        st.dataframe(df3_2)
+        col5, col6 = st.columns(2)
+        
+        with col5:
+            st.dataframe(df3_2)
+        with col6:
+            df1_filtered = df1[df1['paralel']==str(paralell)]
+            st.dataframe(df1_filtered[['matkul','jenis','paralel','hari','jam']])
+        
+        st.subheader("Possible SC Paralel")
+        st.dataframe(df3_3)
