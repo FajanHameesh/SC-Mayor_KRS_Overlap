a95b28823cd80672724e16c121506eb985645f01
FajanHameesh <fajanhameesh@gmail.com>
2026-07-19 07:04:06 +0700

    Cache
    Added Streamlit caching to listMK function.

--- MainFolder/listMK.py
+++ MainFolder/listMK.py
@@ -2,7 +2,9 @@
 
 import requests
 import pandas as pd
+import streamlit as st
 
+@st.cache_data(max_entries=2)
 def listMK():
     use_url = "https://krs.ipb.ac.id/api/StokSupportingCourse?tahunSemesterId=113"
     response = requests.get(use_url)
